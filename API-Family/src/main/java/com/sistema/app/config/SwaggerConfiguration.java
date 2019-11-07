4df60828052a53c560dc28bf6034ee4e06b66435
Kevin Peter Flores Tarmeño <kflorest@everis.com>
2019-11-07 03:18:15 -0500

    Modificacion

--- API-Family/src/main/java/com/sistema/app/config/SwaggerConfiguration.java
+++ API-Family/src/main/java/com/sistema/app/config/SwaggerConfiguration.java
@@ -2,7 +2,6 @@ package com.sistema.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,7 +9,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
-@Configuration
+ 	@Configuration
 	@EnableSwagger2WebFlux
 	public class SwaggerConfiguration {
 

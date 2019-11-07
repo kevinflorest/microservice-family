2af4ee35fe468a8b495373d097bbbd345bd96973
Kevin Peter Flores Tarmeño <kflorest@everis.com>
2019-11-07 11:55:37 -0500

    Modificacion

--- API-Family/src/test/java/com/sistema/app/ApiFamilyApplicationTests.java
+++ API-Family/src/test/java/com/sistema/app/ApiFamilyApplicationTests.java
@@ -18,6 +18,10 @@ public class ApiFamilyApplicationTests {
 	public ApiFamilyApplicationTests() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
+	@Test
+	public void contextLoads() {
+	}
 
 }

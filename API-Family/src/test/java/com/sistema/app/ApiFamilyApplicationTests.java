370374117a74178cbc4320e4996b1d0ae973fb01
Kevin Peter Flores Tarmeño <kflorest@everis.com>
2019-11-07 02:54:11 -0500

    Modificacion

--- API-Family/src/test/java/com/sistema/app/ApiFamilyApplicationTests.java
+++ API-Family/src/test/java/com/sistema/app/ApiFamilyApplicationTests.java
@@ -2,13 +2,18 @@ package com.sistema.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApiFamilyApplicationTests {
 
+	@Autowired
+	private WebTestClient client; 
+	
 	@Test
 	public void contextLoads() {
 	}
